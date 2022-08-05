85be6380f76c1528449dc52167513a36f706c494
gelik <rezervec@mail.ru>
2022-08-05 23:27:38 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+    <h5 style="color:white;">Главная</h5>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+body{background-color: antiquewhite;}
+// @import '~materialize-css\dist\css\materialize.min.css';
+@import 'assets/index.css';
+@import 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css';
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h5>Home Page</h5>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    HelloWorld
+}
+}
+</script>
